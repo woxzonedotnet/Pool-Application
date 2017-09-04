0557f034d565bae2f57d76194d49635735cc3e06
Woxzone-Thameesha <popularitinfo@gmail.com>
2017-09-04 14:09:49 +0530

    report edit

---
+++ Report_Layer/rptInvoice.cs
@@ -0,0 +1,103 @@
+﻿namespace Report_Layer
+{
+    using CrystalDecisions.CrystalReports.Engine;
+    using CrystalDecisions.Shared;
+    using System;
+    using System.ComponentModel;
+
+    public class rptInvoice : ReportClass
+    {
+        [Browsable(false), DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public Section GroupFooterSection1
+        {
+            get
+            {
+                return this.ReportDefinition.Sections[4];
+            }
+        }
+
+        [Browsable(false), DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public Section GroupHeaderSection1
+        {
+            get
+            {
+                return this.ReportDefinition.Sections[2];
+            }
+        }
+
+        [Browsable(false), DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public IParameterField Parameter_strCopyRight
+        {
+            get
+            {
+                return this.DataDefinition.ParameterFields[0];
+            }
+        }
+
+        [Browsable(false), DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public IParameterField Parameter_strInvoiceNo
+        {
+            get
+            {
+                return this.DataDefinition.ParameterFields[1];
+            }
+        }
+
+        public override string ResourceName
+        {
+            get
+            {
+                return "rptInvoice.rpt";
+            }
+            set
+            {
+            }
+        }
+
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden), Browsable(false)]
+        public Section Section1
+        {
+            get
+            {
+                return this.ReportDefinition.Sections[0];
+            }
+        }
+
+        [Browsable(false), DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public Section Section2
+        {
+            get
+            {
+                return this.ReportDefinition.Sections[1];
+            }
+        }
+
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden), Browsable(false)]
+        public Section Section3
+        {
+            get
+            {
+                return this.ReportDefinition.Sections[3];
+            }
+        }
+
+        [Browsable(false), DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public Section Section4
+        {
+            get
+            {
+                return this.ReportDefinition.Sections[5];
+            }
+        }
+
+        [Browsable(false), DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public Section Section5
+        {
+            get
+            {
+                return this.ReportDefinition.Sections[6];
+            }
+        }
+    }
+}
+
